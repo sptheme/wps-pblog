400eb0c52802c0900e67e9892c3b881fafead9c0
sopheak.peas <sopheak.peas@novacambodia.com>
2016-06-04 18:51:06 +0700

    Working with search result page

--- search.php
+++ search.php
@@ -1,39 +1,56 @@
 <?php
 /**
- * The template for displaying search results pages.
+ * The template for displaying Search Results pages.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WPSP_Blog
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+
+			<?php endif; ?>
+
+			<div id="blog-entries" class="<?php wpsp_blog_wrap_classes(); ?>">
+
+				<?php
+				// Define counter for clearing floats
+				$wpsp_count = 0;
+
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+					// Add to counter
+					$wpsp_count++;
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'partials/blog/blog-entry-layout' );
+
+					if ( wpsp_blog_entry_columns() == $wpsp_count ) {
+						$wpsp_count=0;
+					}
+				endwhile; ?>
+
+				<?php
+				// Display post pagination (next/prev - 1,2,3,4..)
+				wpsp_blog_pagination(); ?>
+
+			</div> <!-- #blog-entries -->
 
 		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'wpspblog' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
@@ -41,7 +58,7 @@ get_header(); ?>
 		endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
